a85e06b7478df1379c10a3481a365562cd59f80d
Rikharthu <uberviolence@gmail.com>
2017-12-06 15:02:55 +0200

    added navigation drawer

--- app/src/main/java/com/example/rikharthu/contosouniversity/MainActivity.kt
+++ app/src/main/java/com/example/rikharthu/contosouniversity/MainActivity.kt
@@ -1,46 +1,71 @@
 package com.example.rikharthu.contosouniversity
 
-import android.arch.lifecycle.Observer
+import android.content.res.Configuration
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import android.widget.LinearLayout
-import com.example.rikharthu.contosouniversity.data.models.Course
-import com.example.rikharthu.contosouniversity.data.models.DepartmentWithCourses
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import timber.log.Timber
+import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerToggle: ActionBarDrawerToggle
+
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        toolbar = findViewById(R.id.toolbar)
+        toolbar.title = "Main"
+        setSupportActionBar(toolbar)
+        drawerToggle = setupDrawerToggle()
+        drawerLayout.addDrawerListener(drawerToggle)
+
+        navigation.setNavigationItemSelectedListener(this)
+
         val db = App.get(this).getContosoDatabase()
-        val studentDao = db.studentDao()
-        val students = studentDao.getAll()
         val departments = db.departmentDao().getAll()
-        val instructors = db.instructorDao().getAll()
-        val courses = db.courseDao().getAll()
-
-        val carson = db.studentDao().findByName("Carson", "Alexander")
-        val carsonEnrollments = db.enrollmentDao().findStudentEnrollments(carson.id)
-        val carsonCourses = mutableListOf<Course>()
-        carsonEnrollments.forEach {
-            carsonCourses.add(db.courseDao().findById(it.courseId))
-        }
-
-        val departmentsWithCourses = db.departmentDao().getDepartmentsWithCourses()
-        val departmentsWithCoursesLiveData = db.departmentDao().getDepartmentsWithCoursesLiveData()
-        departmentsWithCoursesLiveData.observe(this, Observer<List<DepartmentWithCourses>> {
-            Timber.d(it.toString())
-        })
-
-        val x = 4
-        Timber.d(x.toString())
 
         val departmentsAdapter = DepartmentsAdapter(departments)
         departmentsRv.adapter = departmentsAdapter
         departmentsRv.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged(newConfig)
+    }
+
+    private fun setupDrawerToggle(): ActionBarDrawerToggle {
+        // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
+        // and will not render the hamburger icon without it.
+        return ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(this, "selected: ${item.itemId}", Toast.LENGTH_SHORT).show()
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (drawerToggle.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
     }
 }
